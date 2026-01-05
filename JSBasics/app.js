6fc8eed62c764f7128c8f46d81cec8474848250e
Shreyasi Ambashta <shreyasi.a0112@gmail.com>
2026-01-05 18:47:24 +0530

    Initial commit- practice folder

---
+++ JSBasics/app.js
@@ -0,0 +1,22 @@
+// let age=prompt("what is your age");
+// switch(age){
+//     case '16':
+//         console.log("Your age is 16")
+//     case '17':
+//         console.log("Your age is 17")
+//     case '18':
+//         console.log("Your age is 18")
+//     break    
+//     case '19':
+//         console.log("Your age is 19")
+//     default:
+//         console.log("Your age is not present")            
+// }
+
+let number=prompt(parseInt("Write number"));
+if (number%2==0 && number%3==0){
+    console.log("Divisible by 2 and 3 both")
+}
+else{
+    console.log("Not divsible")
+}
